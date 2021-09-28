3dc93a23d5bcd78ce708e31b267a6997cd9f1944
Rich Griffiths <rich.griffiths89@gmail.com>
2021-09-28 12:41:39 +0100

    debugging

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.get('/webhook', (req, res) => {
     res.json("Stripe Webhook Endpoint")
 })
 
-app.post('/webhook', (req, res) => { // express.raw({ type: "application/json" })
+app.post('/webhook', async (req, res) => { // express.raw({ type: "application/json" })
     // Ensure call is from stripe
     // const payload = req.body
     // const sig = req.headers['stripe-signature']
@@ -87,22 +87,22 @@ app.post('/webhook', (req, res) => { // express.raw({ type: "application/json" }
     // }
 
     // Handle the event
-    switch (req.type) {
-        case 'payment_intent.created':
-            const paymentCreated = event.data.object;
-            // Then define and call a function to handle the event payment_intent.created
-            processEmail();
-            break;
-        case 'payment_intent.succeeded':
-            const paymentSucceeded = event.data.object;
-            // Then define and call a function to handle the event payment_intent.succeeded
-            break;
-        default:
-            console.log(`Unhandled event type ${event.type}`);
-    }
+    // switch (req.type) {
+    //     case 'payment_intent.created':
+    //         const paymentCreated = event.data.object;
+    //         // Then define and call a function to handle the event payment_intent.created
+    //         processEmail();
+    //         break;
+    //     case 'payment_intent.succeeded':
+    //         const paymentSucceeded = event.data.object;
+    //         // Then define and call a function to handle the event payment_intent.succeeded
+    //         break;
+    //     default:
+    //         console.log(`Unhandled event type ${event.type}`);
+    // }
 
     // Return a 200 response to acknowledge receipt of the event
-    res.send();
+    res.send("Stripe Hit This");
 })
     
 app.listen(process.env.PORT || 3000)
