d91d0bac1275f08373510dc1e561aa6d9a2557c9
Rich Griffiths <rich.griffiths89@gmail.com>
2022-01-11 14:29:35 +0000

    Updated environment

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express()
 
 const cors = require('cors')
 app.use(cors({
-    origin: "https://testpobl.powerappsportals.com", //https://pobl.powerappsportals.com
+    origin: "https://fredpobl.powerappsportals.com/", //https://pobl.powerappsportals.com
 }))
 
 const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
