460b51f0ab6a57d69d194c773e029b98318687dc
Rich Griffiths <rich.griffiths89@gmail.com>
2022-02-18 11:43:14 +0000

    new webhook secret

--- app.js
+++ app.js
@@ -110,8 +110,8 @@ app.post('/webhook', express.raw({ type: "application/json" }), async (req, res)
     // Ensure call is from stripe
     const payload = req.body
     const sig = req.headers['stripe-signature']
-    const endpointSecret = 'whsec_EymoqWPoFlvHQ14v0dm7D8OkLyKtIcbX'
-
+    const endpointSecret = 'whsec_KdRvUcl3lbiqgvLZFViOs5Q7OAeDoiMC'
+                            
     let event;
 
     try {
