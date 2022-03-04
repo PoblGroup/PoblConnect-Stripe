a83ad25bb1263153a83d2d3aa786f67b135106be
Rich Griffiths <rich.griffiths89@gmail.com>
2022-03-04 16:46:33 +0000

    new domain

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express()
 
 const cors = require('cors')
 app.use(cors({
-    origin: "https://poblconnect.powerappsportals.com",
+    origin: "https://poblconnect.co.uk",
 }))
 
 const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
